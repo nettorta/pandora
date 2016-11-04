68ee0ebb29ceadfd8de99b0c563beb865c279d70
Alexey Lavrenuke <direvius@gmail.com>
2016-11-05 01:12:02 +0300

    http post test

--- gun/http/http_test.go
+++ gun/http/http_test.go
@@ -2,6 +2,7 @@ package http
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
 	"testing"
@@ -119,6 +120,75 @@ func TestHttpGunWithHttp(t *testing.T) {
 	select {
 	case r := <-requests:
 		assert.Equal(t, "GET", r.Method)
+		assert.Equal(t, "example.org", r.Host)
+		assert.Equal(t, "/path", r.URL.Path)
+		assert.Equal(t, "Pandora/0.0.1", r.Header.Get("User-Agent"))
+	case <-ctx.Done():
+		t.Fatal(ctx.Err())
+	}
+
+	// TODO: test scenaries with errors
+
+	select {
+	case err := <-promise:
+		require.NoError(t, err)
+	case <-ctx.Done():
+		t.Fatal(ctx.Err())
+	}
+}
+
+func TestHttpGunWithHttpPost(t *testing.T) {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+	defer cancel()
+
+	result := make(chan *aggregate.Sample)
+	requests := make(chan *http.Request)
+
+	ts := httptest.NewServer(
+		http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			fmt.Fprintln(w, "Hello, client")
+			// body is checked here because in the goroutine
+			// the reader is already closed
+			body, err := ioutil.ReadAll(r.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			assert.Equal(t, "Hello server", string(body))
+			go func() {
+				requests <- r
+			}()
+		}))
+	defer ts.Close()
+
+	gun := &HttpGun{
+		target:  ts.Listener.Addr().String(),
+		ssl:     false,
+		results: result,
+	}
+	promise := utils.Promise(func() error {
+		defer close(result)
+		return gun.Shoot(ctx, &ammo.Http{
+			Host:   "example.org",
+			Method: "POST",
+			Uri:    "/path",
+			Body:   "Hello server",
+			Headers: map[string]string{
+				"Accept":          "*/*",
+				"Accept-Encoding": "gzip, deflate",
+				"Host":            "example.org",
+				"User-Agent":      "Pandora/0.0.1",
+			},
+		})
+	})
+	results := aggregate.Drain(ctx, result)
+	require.Len(t, results, 1)
+	assert.Equal(t, "REQUEST", results[0].Tag)
+	assert.Equal(t, 200, results[0].ProtoCode)
+
+	select {
+	case r := <-requests:
+
+		assert.Equal(t, "POST", r.Method)
 		assert.Equal(t, "example.org", r.Host)
 		assert.Equal(t, "/path", r.URL.Path)
 		assert.Equal(t, "Pandora/0.0.1", r.Header.Get("User-Agent"))
